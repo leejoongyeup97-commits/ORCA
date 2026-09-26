7485c242755057a5d473b4ab68f90b1286e8b2e7
pshland <pshland98@gmail.com>
2026-09-26 17:39:59 +0900

    Bump Personal OCR version for new hero batch

--- be/orca_ocr/engine.py
+++ be/orca_ocr/engine.py
@@ -1085,7 +1085,7 @@ def extract_personal(img, hero_key=None):
 
     return {
         'screen_type':'personal',
-        'ocr_version':'0.10.20-dev',
+        'ocr_version':'0.10.21-dev',
         'hero_key':hero_key,
         'hero_name_raw':hero_name_raw,
         'hero_summary_raw':hero_summary_raw,
