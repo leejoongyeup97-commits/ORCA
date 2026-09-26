443440e94c89eb8ddd5090a95e8071e832013d44
pshland <pshland98@gmail.com>
2026-09-26 16:38:58 +0900

    Bump Personal OCR version for D.Mon metrics

--- be/orca_ocr/engine.py
+++ be/orca_ocr/engine.py
@@ -1035,7 +1035,7 @@ def extract_personal(img, hero_key=None):
 
     return {
         'screen_type':'personal',
-        'ocr_version':'0.10.7-dev',
+        'ocr_version':'0.10.8-dev',
         'hero_key':hero_key,
         'hero_name_raw':hero_name_raw,
         'hero_summary_raw':hero_summary_raw,
