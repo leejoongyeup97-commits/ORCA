a01c0277a067889fe310e05283ab4411f26b8a13
pshland <pshland98@gmail.com>
2026-09-26 16:49:53 +0900

    Bump Personal OCR version for Ramattra metrics

--- be/orca_ocr/engine.py
+++ be/orca_ocr/engine.py
@@ -1040,7 +1040,7 @@ def extract_personal(img, hero_key=None):
 
     return {
         'screen_type':'personal',
-        'ocr_version':'0.10.9-dev',
+        'ocr_version':'0.10.10-dev',
         'hero_key':hero_key,
         'hero_name_raw':hero_name_raw,
         'hero_summary_raw':hero_summary_raw,
